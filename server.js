77caea44e25af79e5656f1bb8891e55537870538
DogukanErgin <d35ergin@hotmail.com>
2022-07-22 18:59:17 +0300

    update

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/angularexample1_kartvizit'));
+app.use(express.static(__dirname + '/dist/AngularExample1_Kartvizit'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/angularexample1_kartvizit/'));});
+'/dist/AngularExample1_Kartvizit/index.html'));});
 app.listen(process.env.PORT || 8080);
